078d384f46c9611ef0be5d0e3b98cb1fcdc81955
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2021-09-17 12:33:21 -0300

    :hammer: correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  important: true,
+  // Active dark mode on class basis
+
     mode: 'jit',
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -15,8 +18,40 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+
+            screens: {
+              'print': {'raw': 'print'},
+              // => @media print { ... }
+              'sm': '640px',
+              // => @media (min-width: 640px) { ... }
+
+              'md': '768px',
+              // => @media (min-width: 768px) { ... }
+
+              'lg': '1024px',
+              // => @media (min-width: 1024px) { ... }
+
+              'xl': '1280px',
+              // => @media (min-width: 1280px) { ... }
+
+            },
+            backgroundImage: (theme) => ({
+              check: "url('/icons/check.svg')",
+              landscape: "url('/images/landscape/2.jpg')",
+            }),
         },
     },
-
+    variants: {
+      extend: {
+        backgroundColor: ["checked"],
+        borderColor: ["checked"],
+        inset: ["checked"],
+        zIndex: ["hover", "active"],
+      },
+    },
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    future: {
+      purgeLayersByDefault: true,
+    },
+    darkMode: "class",
 };
